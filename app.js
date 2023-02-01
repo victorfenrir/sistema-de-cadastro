1d23458e32b2e7d0709128f6e6813a971e7f9d97
Victor Barra <victorbarrapereira@gmail.com>
2023-02-01 11:05:46 -0300

    teste bem-sucedido

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 //mais um console log de teste:
 console.log("o paulo fez esse commit sem você ver na aula!")
+//outro teste, dessa vez para verificar sync do VCS com o GitHub:
+console.log("teste deu certo, usuário verificado")

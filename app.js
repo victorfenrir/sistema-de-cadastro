dbe4b36f24d3cdc160078024d7b8a569af9bae8e
Victor Barra <victorbarrapereira@gmail.com>
2023-02-01 14:25:41 -0300

    possível teste final

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ console.log("aplicação está se conectando ao banco de dados")
 //mais um console log de teste:
 console.log("o paulo fez esse commit sem você ver na aula!")
 //outro teste, dessa vez para verificar sync do VCS com o GitHub:
-console.log("teste deu errado, usuário não-verificado")
+console.log("teste final?")
